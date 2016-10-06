aed3eb41106bd289d2ea5696b87fcb576b1df17c
Victor R. Huerta Hernández <vhuertahnz@gmail.com>
2016-10-06 17:21:51 -0500

    Intentional delay to test travis and heroku integration

--- test/math.test.js
+++ test/math.test.js
@@ -5,13 +5,12 @@ import math from './../src/math';
 describe('Testing math', () => {
 
   it('should return 10', () => {
-    math.sum(5, 5)
-      .should.equal(10);
+    math.sum(5, 5).should.equal(10);
   });
 
-  it('should return 10', () => {
-    math.div(20, 2)
-      .should.equal(10);
+  it('should return 10', done => {
+    math.div(20, 2).should.equal(10);
+    setTimeout(done, 60 * 1000);
   });
 
 });
